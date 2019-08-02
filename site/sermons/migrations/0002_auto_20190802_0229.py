eb68569745857ab3c766691c85a4aa5b929085e0
Benjamin Locher <ben@engagedc.com>
2019-08-01 22:30:45 -0400

    removing uploads from git

--- site/sermons/migrations/0002_auto_20190802_0229.py
+++ site/sermons/migrations/0002_auto_20190802_0229.py
@@ -5,14 +5,14 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('sermons', '0001_initial'),
-    ]
+    dependencies = [("sermons", "0001_initial")]
 
     operations = [
         migrations.AlterField(
-            model_name='sermon',
-            name='text',
-            field=models.TextField(blank=True, help_text='The full content of the sermon', null=True, verbose_name='Text'),
-        ),
+            model_name="sermon",
+            name="text",
+            field=models.TextField(
+                blank=True, help_text="The full content of the sermon", null=True, verbose_name="Text"
+            ),
+        )
     ]
