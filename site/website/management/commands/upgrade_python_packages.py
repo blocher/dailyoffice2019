b2d6c5ad469c677cd0113b609340b7268894cc7e
Benjamin Locher <ben@engagedc.com>
2019-08-17 16:56:13 -0400

    upgrading pythong packages

---
+++ site/website/management/commands/upgrade_python_packages.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+import pkg_resources
+from subprocess import call
+
+
+class Command(BaseCommand):
+    help = "Update all python packages and write to requirements.txt"
+
+    def handle(self, *args, **options):
+
+        packages = [dist.project_name for dist in pkg_resources.working_set]
+        call("pip install --upgrade --upgrade-strategy=eager " + " ".join(packages), shell=True)
+        call("pip freeze > ../requirements.txt", shell=True)
