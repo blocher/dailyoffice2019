9985764d58953006a63ecb179c2c1d7c2ba4814f
Benjamin Locher <ben@engagedc.com>
2019-08-27 21:33:53 -0400

    bible app refactoring

--- site/bible/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BibleConfig(AppConfig):
-    name = "bible"
--- site/website/settings.py
+++ site/website/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
