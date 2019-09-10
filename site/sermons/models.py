17a65152d772a6dbf5794b081100356b40ef6535
Benjamin Locher <ben@engagedc.com>
2019-09-09 21:24:08 -0400

    keyword WIP

--- site/sermons/models.py
+++ site/sermons/models.py
@@ -160,27 +160,27 @@ class Sermon(UUIDModel):
         return None
 
     def getKeyWords(self):
-        return
-        # import pke
-        #
-        # # initialize keyphrase extraction model, here TopicRank
-        # extractor = pke.unsupervised.TfIdf()
-        #
-        # # load the content of the document, here document is expected to be in raw
-        # # format (i.e. a simple text file) and preprocessing is carried out using spacy
-        # extractor.load_document(input=self.text, language="en")
-        #
-        # # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
-        # # and adjectives (i.e. `(Noun|Adj)*`)
-        # extractor.candidate_selection()
-        #
-        # # candidate weighting, in the case of TopicRank: using a random walk algorithm
-        # extractor.candidate_weighting()
-        #
-        # # N-best selection, keyphrases contains the 10 highest scored candidates as
-        # # (keyphrase, score) tuples
-        # keyphrases = extractor.get_n_best(n=10)
-        # print(keyphrases)
+        import pke
+
+        # initialize keyphrase extraction model, here TopicRank
+        extractor = pke.unsupervised.KPMiner()
+
+        # load the content of the document, here document is expected to be in raw
+        # format (i.e. a simple text file) and preprocessing is carried out using spacy
+
+        extractor.load_document(input=self.text, language="en")
+
+        # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
+        # and adjectives (i.e. `(Noun|Adj)*`)
+        extractor.candidate_selection()
+
+        # candidate weighting, in the case of TopicRank: using a random walk algorithm
+        extractor.candidate_weighting()
+
+        # N-best selection, keyphrases contains the 10 highest scored candidates as
+        # (keyphrase, score) tuples
+        keyphrases = extractor.get_n_best(n=10)
+        print(keyphrases)
 
     def save(self, *args, **kwargs):
         if self.file:
