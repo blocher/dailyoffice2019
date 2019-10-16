c67e55b42ce224020b75f1216eb207b26fd853d2
Benjamin Locher <ben@engagedc.com>
2019-10-16 13:42:21 -0400

    importing special days

--- site/office/management/commands/import_office_days.py
+++ site/office/management/commands/import_office_days.py
@@ -6,7 +6,8 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 from bible.sources import PassageNotFoundException
-from office.models import OfficeDay
+from churchcal.models import Commemoration
+from office.models import StandardOfficeDay, HolyDayOfficeDay
 from bible import Passage
 
 
@@ -30,6 +31,8 @@ class Command(BaseCommand):
     SHEET_ID = "1s7GqYoy3HC5JD64opdRldAAi3mwsSQxtC6ZzzF2yUAg"
     RANGE_NAME = "OfficeDates!A3:K368"
 
+    HOLY_DAY_RANGE_NAME = "OfficeDatesHolyDays!A3:G9"
+
     def parse_passage(self, passage):
         try:
             passage = scriptures.extract(passage)[0]
@@ -39,6 +42,7 @@ class Command(BaseCommand):
 
     def get_passage(self, passage):
         passage = self.parse_passage(passage)
+        print(Passage(passage, source="esv").html)
         try:
             return Passage(passage, source="esv").html
         except PassageNotFoundException:
@@ -60,27 +64,50 @@ class Command(BaseCommand):
             result = sheet.values().get(spreadsheetId=self.SHEET_ID, range=self.RANGE_NAME).execute()
         except HttpError as e:
             print(e)
-            print("Data not found for this denomination")
+            print("Worksheet not found")
             return
 
         for row in result.get("values", []):
-            day = OfficeDay.objects.get_or_create(month=row[0], day=row[1])
+            day = StandardOfficeDay.objects.get_or_create(month=row[0], day=row[1])
             day = day[0]
             day.month = row[0]
             day.day = row[1]
-            day.holy_day = row[2]
+            day.holy_day_name = row[2]
             day.mp_psalms = row[3].replace(" ", "")
             day.mp_reading_1 = self.parse_passage(row[4])
-            # day.mp_reading_1_text = self.get_passage(row[4])
+            day.mp_reading_1_text = self.get_passage(row[4])
             day.mp_reading_1_abbreviated = row[5]
             day.mp_reading_2 = self.parse_passage(row[6])
-            # day.mp_reading_2_text = self.get_passage(row[6])
+            day.mp_reading_2_text = self.get_passage(row[6])
             day.ep_psalms = row[7].replace(" ", "")
             day.ep_reading_1 = self.parse_passage(row[8])
-            # day.ep_reading_1_text = self.get_passage(row[8])
+            day.ep_reading_1_text = self.get_passage(row[8])
             day.ep_reading_1_abbreviated = row[9]
             day.ep_reading_2 = self.parse_passage(row[10])
-            # day.ep_reading_2_text = self.get_passage(row[10])
+            day.ep_reading_2_text = self.get_passage(row[10])
+            day.save()
+
+        try:
+            result = sheet.values().get(spreadsheetId=self.SHEET_ID, range=self.HOLY_DAY_RANGE_NAME).execute()
+        except HttpError as e:
+            print(e)
+            print("Worksheet for Holy Days not found")
+            return
+
+        for row in result.get("values", []):
+            commemoration = Commemoration.objects.filter(calendar__abbreviation="ACNA_BCP2019", name=row[0]).first()
+            day = HolyDayOfficeDay.objects.get_or_create(commemoration_id=commemoration.pk)[0]
+            day.holy_day_name = row[0]
+            day.mp_psalms = row[1].replace(" ", "")
+            day.mp_reading_1 = self.parse_passage(row[2])
+            day.mp_reading_1_text = self.get_passage(row[2])
+            day.mp_reading_2 = self.parse_passage(row[3])
+            day.mp_reading_2_text = self.get_passage(row[3])
+            day.ep_psalms = row[4].replace(" ", "")
+            day.ep_reading_1 = self.parse_passage(row[5])
+            day.ep_reading_1_text = self.get_passage(row[5])
+            day.ep_reading_2 = self.parse_passage(row[6])
+            day.ep_reading_2_text = self.get_passage(row[6])
             day.save()
 
         print("All done")
