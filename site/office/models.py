bfc926236cb88dd26fee57dd9c801c3716d7b09f
Benjamin Locher <ben@engagedc.com>
2019-10-16 13:42:21 -0400

    importing special days

--- site/office/models.py
+++ site/office/models.py
@@ -1,15 +1,13 @@
 from django.db import models
 
 from churchcal.base_models import BaseModel
+from churchcal.models import Commemoration
 
 
 class OfficeDay(BaseModel):
 
-    month = models.IntegerField()
-    day = models.IntegerField()
-    holy_day = models.CharField(max_length=255, null=True, blank=True)
+    holy_day_name = models.CharField(max_length=255, null=True, blank=True)
     mp_psalms = models.CharField(max_length=255)
-    mp_psalms_text = models.TextField(blank=True, null=True)
     mp_reading_1 = models.CharField(max_length=255)
     mp_reading_1_text = models.TextField(blank=True, null=True)
     mp_reading_1_abbreviated = models.CharField(max_length=255, null=True, blank=True)
@@ -17,10 +15,20 @@ class OfficeDay(BaseModel):
     mp_reading_2 = models.CharField(max_length=255)
     mp_reading_2_text = models.TextField(blank=True, null=True)
     ep_psalms = models.CharField(max_length=255)
-    ep_psalms_text = models.TextField(blank=True, null=True)
     ep_reading_1 = models.CharField(max_length=255)
     ep_reading_1_text = models.TextField(blank=True, null=True)
     ep_reading_1_abbreviated = models.CharField(max_length=255, null=True, blank=True)
     ep_reading_1_abbreviated_text = models.TextField(blank=True, null=True)
     ep_reading_2 = models.CharField(max_length=255)
     ep_reading_2_text = models.TextField(blank=True, null=True)
+
+
+class StandardOfficeDay(OfficeDay):
+
+    month = models.IntegerField()
+    day = models.IntegerField()
+
+
+class HolyDayOfficeDay(OfficeDay):
+
+    commemoration = models.ForeignKey(Commemoration, on_delete=models.CASCADE)
