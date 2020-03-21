49c69c0526baab75f206f1d759fc05675de735c8
Benjamin Locher <blocher@gmail.com>
2020-03-21 16:23:48 -0400

    removing toolbar with scroll

--- site/office/src/office/js/index.js
+++ site/office/src/office/js/index.js
@@ -5,8 +5,10 @@ import {settings} from "./settings";
 import {voice} from "./voice";
 import {setupCalendar} from "./calendar";
 import "../css/index.scss";
+import {handleScrolling} from "./scrolling";
 
 setupRedirect();
 settings();
 voice();
 setupCalendar();
+handleScrolling();
