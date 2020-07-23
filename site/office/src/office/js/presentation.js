e5d7665e87028f77e0da385b0eb4800d85415112
Benjamin Locher <blocher@gmail.com>
2020-07-23 18:20:47 -0400

    fixing safari

--- site/office/src/office/js/presentation.js
+++ site/office/src/office/js/presentation.js
@@ -42,7 +42,6 @@ const scrollPageToPosition = async function scrollToPosition(position) {
 const showHidePaginationLinks = () => {
     const allPages  = document.getElementById('allPages').innerText
     const currentPage = document.getElementById('pageNumber').innerText
-    console.log(allPages, currentPage)
     if (currentPage == "1") {
         document.getElementsByClassName('scroll-back').forEach(elem => {
             elem.classList.add("off")
@@ -75,8 +74,10 @@ const scrollForward = (event) => {
             showHidePaginationLinks();
             enablePaginationEventListeners();
         });
-        event.stopPropagation();
-        event.preventDefault();
+        if (event) {
+            event.stopPropagation();
+            event.preventDefault();
+        }
         return false;
 }
 
@@ -87,8 +88,10 @@ const scrollBackward = (event) => {
             showHidePaginationLinks();
             enablePaginationEventListeners();
     });
-    event.stopPropagation();
-    event.preventDefault();
+    if (event) {
+        event.stopPropagation();
+        event.preventDefault();
+    }
     return false;
 }
 
@@ -133,7 +136,71 @@ const deincrementPageNumber = () => {;
     document.getElementById('pageNumber').innerText = pageNumber.toString()
 }
 
+const setupSwipes = () => {
+    let touchstartX = 0;
+    let touchstartY = 0;
+    let touchendX = 0;
+    let touchendY = 0;
+
+    const gestureZone = document.getElementById('web');
+
+    gestureZone.addEventListener('touchstart', function(event) {
+        touchstartX = event.changedTouches[0].screenX;
+        touchstartY = event.changedTouches[0].screenY;
+    }, false);
+
+    gestureZone.addEventListener('touchend', function(event) {
+        touchendX = event.changedTouches[0].screenX;
+        touchendY = event.changedTouches[0].screenY;
+        handleSwipe();
+    }, false);
+
+    const handleSwipe = () => {
+        if (touchendX < touchstartX) {
+            scrollForward()
+        }
+
+        if (touchendX > touchstartX) {
+            scrollBackward()
+        }
+    }
+
+    function handleGesture() {
+        if (touchendX < touchstartX) {
+            console.log('Swiped left');
+        }
+
+        if (touchendX > touchstartX) {
+            console.log('Swiped right');
+        }
+
+        if (touchendY < touchstartY) {
+            console.log('Swiped up');
+        }
+
+        if (touchendY > touchstartY) {
+           console.log('Swiped down');
+        }
+
+        if (touchendY === touchstartY) {
+           console.log('Tap');
+        }
+    }
+}
+
 const initialize = () => {
+    document.getElementById('web').addEventListener('touchmove', (event)=> {
+        event.preventDefault();
+        event.stopPropagation();
+    }, {passive: false});
+    document.getElementById('web').addEventListener('touchstart', (event)=> {
+        event.preventDefault();
+        event.stopPropagation();
+    }, {passive: false});
+    document.getElementById('web').addEventListener('touchend', (event)=> {
+        event.preventDefault();
+        event.stopPropagation();
+    }, {passive: false});
     document.getElementsByTagName('body')[0].classList.add("no-scroll")
     document.getElementsByClassName("no-print").forEach((elem) => {
         elem.classList.add('off');
@@ -146,12 +213,11 @@ const initialize = () => {
     // });
     enablePaginationEventListeners();
 
-    const desiredWidth = Math.max(Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),580);
-    const desiredHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 100 - convertRemToPixels(2);
+    const desiredWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    const desiredHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 70;
     const mainDiv = document.getElementById('web');
-    const totalHeight = mainDiv.offsetHeight;
+    const totalHeight = mainDiv.clientHeight;
     const pageCount = Math.floor(totalHeight/desiredHeight) + 1;
-    //alert("A" + pageCount)
     const width = desiredWidth * pageCount;
     mainDiv.style.margin = "20px 0";
     mainDiv.style.padding = "10px 0px"; //(optional) prevents clipped letters around the edges
@@ -162,7 +228,9 @@ const initialize = () => {
     mainDiv.style.columnFill = "auto";
     mainDiv.style.columnGap = "0px";
     document.getElementById('allPages').innerText = pageCount.toString()
+    setupSwipes()
     showHidePaginationLinks()
+
 }
 
 const reinitialize = () => {
@@ -173,6 +241,7 @@ const reinitialize = () => {
     mainDiv.style.width = "auto";
     mainDiv.style.height = "auto";
     mainDiv.style.columnWidth = "auto";
+    document.getElementById('pageNumber').innerText = "1"
     initialize()
 
 }
