34397c6836a0a531976f2d43407ca64a34dcf906
Benjamin Locher <blocher@gmail.com>
2020-09-20 23:08:04 -0400

    year and month views

--- site/churchcal/api/views.py
+++ site/churchcal/api/views.py
@@ -1,10 +1,13 @@
+from calendar import monthrange
+
+from django.core.cache import cache
 from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from churchcal.api.permissions import ReadOnly
 from churchcal.api.serializer import DaySerializer
-from churchcal.calculations import get_calendar_date
+from churchcal.calculations import get_calendar_date, ChurchYear
 
 
 class DayView(APIView):
@@ -14,4 +17,29 @@ class DayView(APIView):
         date = timezone.now().replace(year=year, month=month, day=day)
         calendar_date = get_calendar_date(date)
         serializer = DaySerializer(calendar_date)
+        return Response(serializer.data)
+
+
+class MonthView(APIView):
+    permission_classes = [ReadOnly]
+
+    def get(self, request, year, month):
+        dayrange = monthrange(year, month)
+        days = [
+            get_calendar_date(timezone.now().replace(year=year, month=month, day=day))
+            for day in range(1, dayrange[1] + 1)
+        ]
+        serializer = DaySerializer(days, many=True)
+        return Response(serializer.data)
+
+
+class YearView(APIView):
+    permission_classes = [ReadOnly]
+
+    def get(self, request, year):
+        church_year = cache.get(str(year))
+        if not church_year:
+            church_year = ChurchYear(year)
+            cache.set(str(year), church_year, 60 * 60 * 12)
+        serializer = DaySerializer([date for date in church_year], many=True)
         return Response(serializer.data)
--- site/website/api_urls.py
+++ site/website/api_urls.py
@@ -2,12 +2,14 @@ from rest_framework import routers
 from django.urls import include, path, re_path
 from rest_framework import routers
 
-from churchcal.api.views import DayView
+from churchcal.api.views import DayView, MonthView, YearView
 
 router_v1 = routers.DefaultRouter()
 
 urlpatterns = [
     re_path(r"^api/v1/", include(router_v1.urls)),
-    path('api/v1/api-auth/', include('rest_framework.urls')),
+    path("api/v1/api-auth/", include("rest_framework.urls")),
     path(r"api/v1/calendar/<int:year>-<int:month>-<int:day>", DayView.as_view(), name="day_view"),
+    path(r"api/v1/calendar/<int:year>-<int:month>", MonthView.as_view(), name="month_view"),
+    path(r"api/v1/calendar/<int:year>", YearView.as_view(), name="month_view"),
 ]
