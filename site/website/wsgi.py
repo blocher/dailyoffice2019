94c2775fcb3be08c94929e1c2e5b89ba776a0264
Benjamin Locher <blocher@gmail.com>
2020-09-22 23:15:58 -0400

    wsgi.py updates

--- site/website/wsgi.py
+++ site/website/wsgi.py
@@ -8,12 +8,27 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+sys.path.append("/var/www/dailyoffice2019/site")
+sys.path.append("/var/www/dailyoffice2019/env/lib/python3.8/site-packages")
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if "mod_wsgi" in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
