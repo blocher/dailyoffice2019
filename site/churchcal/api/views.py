69324fbc632168a241accb4568ae7144424c0844
Benjamin Locher <blocher@gmail.com>
2020-09-28 21:50:02 -0400

    adding settings parsing

--- site/churchcal/api/views.py
+++ site/churchcal/api/views.py
@@ -1,6 +1,7 @@
 from calendar import monthrange
 
 from django.core.cache import cache
+from django.http import HttpResponse
 from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -42,3 +43,70 @@ class YearView(APIView):
             cache.set(str(year), church_year, 60 * 60 * 12)
         serializer = DaySerializer([date for date in church_year], many=True)
         return Response(serializer.data)
+
+
+class Settings(object):
+
+    DEFAULT_SETTINGS = {
+        "setting_psalter": "60",
+        "setting_reading_cycle": "1",
+        "setting_reading_length": "full",
+        "setting_reading_audio": "off",
+        "setting_canticle_rotation": "default",
+        "setting_theme": "theme-auto",
+        "setting_lectionary": "daily-office-readings",
+        "setting_confession": "long-on-fast",
+        "setting_absolution": "lay",
+        "setting_morning_prayer_invitatory": "invitatory_traditional",
+        "setting_reading_headings": "off",
+        "setting_language_style": "traditional",
+        "setting_national_holidays": "all",
+        "setting_suffrages": "rotating",
+        "setting_collects": "rotating",
+        "setting_pandemic_prayers": "pandemic_yes",
+        "setting_mp_great_litany": "mp_litany_off",
+        "setting_ep_great_litany": "ep_litany_off",
+        "setting_general_thanksgiving": "on",
+        "setting_chrysostom": "on",
+        "setting_grace": "rotating",
+        "setting_o_antiphons": "literal",
+    }
+
+    def __init__(self, request):
+        self.settings = self._get_settings(request)
+
+    def get_setting(self, name):
+        name = name.lower()
+        try:
+            return self.settings[name]
+        except KeyError:
+            return False
+
+    def __getitem__(self, key):
+        key = key.lower()
+        try:
+            return self.settings[key]
+        except KeyError:
+            return False
+
+    def _get_settings(self, request):
+
+        settings = self.DEFAULT_SETTINGS
+        specified_settings = {k: v for (k, v) in request.query_params.items() if k in settings.keys()}
+        for k, v in settings.items():
+            if k in specified_settings.keys():
+                settings[k] = specified_settings[k]
+        return settings
+
+
+class OfficeAPIView(APIView):
+    permission_classes = [ReadOnly]
+
+    def get(self, request, year, month, day):
+        settings = Settings(request)
+        return HttpResponse()
+
+
+class MorningPrayer(OfficeAPIView):
+    def get(self, request, year, month, day):
+        return super().get(request, year, month, day)
--- site/website/api_urls.py
+++ site/website/api_urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from churchcal.api.views import DayView, MonthView, YearView
+from churchcal.api.views import DayView, MonthView, YearView, MorningPrayer
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -28,8 +28,12 @@ urlpatterns = [
     path(r"api/v1/calendar/<int:year>-<int:month>-<int:day>", DayView.as_view(), name="day_view"),
     path(r"api/v1/calendar/<int:year>-<int:month>", MonthView.as_view(), name="month_view"),
     path(r"api/v1/calendar/<int:year>", YearView.as_view(), name="month_view"),
-    url(r"^api/v1/openapi(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path(
+        r"api/v1/office/morning_prayer/<int:year>-<int:month>-<int:day>",
+        MorningPrayer.as_view(),
+        name="morning_prayer_view",
+    ),
+    url(r"^api/openapi(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     url(r"^api/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    url(r"^api/v1/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    url(r"^api/v1/redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    url(r"^api/redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
