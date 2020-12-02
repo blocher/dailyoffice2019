33a131a14f54606885544cde559ca2c5d5220a74
Benjamin Locher <blocher@gmail.com>
2020-12-02 17:48:02 -0500

    adding space before Amen.

--- site/website/settings.py
+++ site/website/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY", "TESTKEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
+# SECURITY WARNING: d
+# Don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 DEBUG_DATES = env("DEBUG_DATES")
 MODE = env("MODE")
