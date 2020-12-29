3559e503255aed1605d7950d07264f28c6c2aed4
Benjamin Locher <blocher@gmail.com>
2020-12-28 23:12:35 -0500

    adding asgi config

--- site/website/asgi.py
+++ site/website/asgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for sermons project.
+ASGI config for sermons project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
+It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/asgi/
 """
 
 import os
@@ -13,7 +13,7 @@ import traceback
 import signal
 import sys
 
-from django.core.wsgi import get_wsgi_application
+from django.core.asgi import get_asgi_application
 
 sys.path.append("/var/www/api.dailyoffice2019.com/site")
 sys.path.append("/var/www/api.dailyoffice2019.com/env/lib/python3.9/site-packages")
@@ -22,10 +22,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
 
 
 try:
-    application = get_wsgi_application()
+    application = get_asgi_application()
 except Exception:
     # Error loading applications
-    if "mod_wsgi" in sys.modules:
+    if "mod_asgi" in sys.modules:
         traceback.print_exc()
         os.kill(os.getpid(), signal.SIGINT)
         time.sleep(2.5)
