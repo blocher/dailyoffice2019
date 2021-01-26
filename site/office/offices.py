2a76b470e1ee84e2dac21e55f6837f0767e74e86
Benjamin Locher <blocher@gmail.com>
2021-01-26 08:48:11 -0500

    fixing president

--- site/office/offices.py
+++ site/office/offices.py
@@ -456,7 +456,7 @@ class GreatLitany(OfficeSection):
 
     def get_leaders(self):
         parts = [
-            '<span class="us">your servant Donald Trump, the President, </span>',
+            '<span class="us">your servant Joe Biden, the President, </span>',
             '<span class="canada">your servants Her Majesty Queen Elizabeth, the Sovereign, and Justin Trudeau, the Prime Minister, </span>'
             '<span class="national_none">your servants, our national leaders, </span>',
         ]
