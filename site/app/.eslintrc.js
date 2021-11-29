ee734c2af9c566391cad4d4176cb06e1638fdf9d
Benjamin Locher <blocher@gmail.com>
2021-11-28 22:21:13 -0500

    running eslint

--- site/app/.eslintrc.js
+++ site/app/.eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
