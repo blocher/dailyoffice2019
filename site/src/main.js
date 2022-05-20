c91e07816945a115b3bf79897411ee0c55df19b7
Benjamin Locher <blocher@gmail.com>
2022-05-19 23:36:26 -0400

    removing extra files

--- site/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
-import { loadFonts } from "./plugins/webfontloader";
-
-loadFonts();
-
-createApp(App).use(vuetify).mount("#app");
