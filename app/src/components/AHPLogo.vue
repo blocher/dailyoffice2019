f4127b54ac4bfaa9f0d6e38386391dacdae4288d
Benjamin Locher <blocher@gmail.com>
2024-11-06 17:58:32 -0500

    updating logo

--- app/src/components/AHPLogo.vue
+++ app/src/components/AHPLogo.vue
@@ -1,16 +1,17 @@
 <template>
-  <a
-      href="https://anglicanhousepublishers.org/book-of-common-prayer/" target="_blank">
+  <a href="https://anglicanhousepublishers.org/the-shop/" target="_blank">
     <div class="logoDiv text-center border-2 p-4 bg-white">
-
       <p>This site is generously supported by Anglican House Publishers</p>
-      <img src="@/assets/ahp.png" class="mx-auto flex h-24 mt-6 mb-3">
-      <p>Publisher of the<br><strong>Anglican Church in North America's<br>2019 Edition of the Book of Common
-        Prayer</strong>,<br>on which this website is based.</p>
+      <img src="@/assets/ahp.png" class="mx-auto flex h-24 mt-6 mb-3" />
+      <p>
+        Publisher of the<br /><strong
+          >Anglican Church in North America's<br />2019 Edition of the Book of
+          Common Prayer</strong
+        >,<br />on which this website is based.
+      </p>
       <p><small>Click to purchase a paper copy.</small></p>
     </div>
   </a>
-
 </template>
 
 <script>
@@ -23,7 +24,5 @@ export default {
 .logoDiv {
   max-width: 500px;
   margin: 200px auto;
-
 }
 </style>
-
