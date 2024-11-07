5f658076eeb892878630e32f31bd96d40b0fd8a0
Benjamin Locher <blocher@gmail.com>
2024-11-07 09:42:48 -0500

    updating image name

--- app/src/components/AHPLogo.vue
+++ app/src/components/AHPLogo.vue
@@ -2,7 +2,7 @@
   <a href="https://anglicanhousepublishers.org/the-shop/" target="_blank">
     <div class="logoDiv text-center border-2 p-4 bg-white">
       <p>This site is generously supported by Anglican House Publishers</p>
-      <img src="@/assets/ahp.png" class="mx-auto flex h-24 mt-6 mb-3" />
+      <img src="@/assets/ahp_new.png" class="mx-auto flex h-24 mt-6 mb-3" />
       <p>
         Publisher of the<br /><strong
           >Anglican Church in North America's<br />2019 Edition of the Book of
