8f36875c5e82cd31bf1b86089fe25d4f3a296c4e
Benjamin Locher <blocher@gmail.com>
2024-12-22 16:58:46 -0500

    fixing unit tests

--- app/tests/unit/example.spec.js
+++ app/tests/unit/example.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import HelloWorld from '../../src/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
---
+++ app/vitest.config.ts
@@ -0,0 +1,12 @@
+// vitest.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.js', // Optional: setup file for test configuration
+  },
+});
