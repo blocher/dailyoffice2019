36d425e60fc4b9c54a6c30869d80b78a95b9efba
Benjamin Locher <blocher@gmail.com>
2025-10-01 11:22:16 -0400

    upgrading gradle

--- app/ios/App/CapApp-SPM/Package.swift
+++ app/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.4.2"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "7.4.3"),
         .package(name: "CapacitorFirebaseAnalytics", path: "../../../node_modules/@capacitor-firebase/analytics"),
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
         .package(name: "CapacitorClipboard", path: "../../../node_modules/@capacitor/clipboard"),
