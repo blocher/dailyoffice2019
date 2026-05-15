722fe8333b0a2055888dd413412eb9576037a1db
Benjamin Locher <blocher@gmail.com>
2026-05-14 22:12:17 -0400

    email list fixes

--- site/standrew/utils.py
+++ site/standrew/utils.py
@@ -14,7 +14,7 @@ from website.settings import SITE_ADDRESS, ZOOM_LINK, DEBUG
 def get_today():
     if DEBUG:
         date = datetime.datetime.strptime(
-            "{} {} {} {} {} {}".format(1, 27, 2026, 6, 00, "AM"),
+            "{} {} {} {} {} {}".format(5, 21, 2026, 6, 00, "AM"),
             "%m %d %Y %I %M %p",
         )
         date = make_aware(date)
